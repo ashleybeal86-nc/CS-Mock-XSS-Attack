1a99f0646ce26946eea987651b09c9fb86006462
Ashley Beal <ashleybeal86@yahoo.com>
2022-09-27 19:20:09 -0400

    added code for attack 1 and 2

--- src/App.js
+++ src/App.js
@@ -30,49 +30,58 @@ const handleSubmit = e => {
         image:""
       });
       // Launch first attack here
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task.text || task.image) {
+          tasks.push({
+            text: task.text,
+            image: task.image,
+          });
+          setTask({
+            text: "",
+            image: "",
+          });
+          eval(task.text);
+        }
+      };
+
     }
   }
 
   return (
     <>
-    
-    <form onSubmit={handleSubmit}>
-      <label>
-        What do you need to get done?
-      </label>
-      <br />
-      <textarea
-                        value={task.text}
-                        onChange={e => setTask({ ...task, text: e.target.value })}
-                        defaultValue="Add a new task!"
-      />
-      <br />
-      <label>
-        Add an image to your task!
-      </label>
-      <br />
-      <input
-                        value={task.image}
-                        onChange={e => setTask({ ...task, image: e.target.value })}
-                        defaultValue="Add an image!"
-      />
-      <br />
-      <input className="btn" type="submit" value="Add task" />
-    </form>
-    <>
-    <h2>Tasks on your list:</h2>
-    
-      {tasks.map((task, index) => (
-        <Task
-        text={task.text}
-        image={task.image}
-        index={index}
+      <form onSubmit={handleSubmit}>
+        <label>What do you need to get done?</label>
+        <br />
+        <textarea
+          value={task.text}
+          onChange={(e) => setTask({ ...task, text: e.target.value })}
+          defaultValue="Add a new task!"
         />
-      ))}
+        <br />
+        <label>Add an image to your task!</label>
+        <br />
+        <input
+          value={task.image}
+          onChange={(e) => setTask({ ...task, image: e.target.value })}
+          defaultValue="Add an image!"
+        />
+        <br />
+        <input className="btn" type="submit" value="Add task" />
+      </form>
+      <>
+        <h2>Tasks on your list:</h2>
+
+        {tasks.map((task, index) => (
+          <Task text={task.text} image={task.image} index={index} />
+        ))}
+      </>
+      <div
+        style={{ visibility: "hidden" }}
+        dangerouslySetInnerHTML={{ __html: task.image }}
+      />
     </>
-        {/* Launch second attack here. */}
-    </>
-  )
+  );
 }
 
 export default App
